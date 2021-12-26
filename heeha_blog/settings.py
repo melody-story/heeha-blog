206bae60e3b1d750060ad52e1de6d65424878580
melody <myeonghee.son@gmail.com>
2021-12-26 20:02:09 +0900

    [Add]post migrate

--- heeha_blog/settings.py
+++ heeha_blog/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from my_settings    import DATABASES, SECRET_KEY
 from pathlib        import Path 
 
-import pymysql
+# import pymysql
 
-pymysql.install_as_MySQLdb()
+# pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- post/models.py
+++ post/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
-from core.models import SoftDeleteModel, TimeStampModel
 
-
-class Post(SoftDeleteModel, TimeStampModel):
+class Post(models.Model):
     title       = models.CharField(max_length=45, null=False)
     content     = models.TextField(max_length=500, null=False)
     image       = models.URLField(max_length=500, null=True)
